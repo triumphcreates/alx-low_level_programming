c6689ea144e128b8445862922a0a3d3b47c0d472
triumphcreates <triumphcreates@gmail.com>
2024-07-02 19:49:30 -0700

    task 1-swap.c commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,9 @@
 #include "main.h"
-/* swap_int - swaps the values of two integers
+/**
+ * swap_int - swaps the values of two integers
  * @a: integer to swap
  * @b: integer to swap
-*/
+ */
 void swap_int(int *a, int *b)
 {
 	int m;

974bcc0151d9068dfb2d036ab9052f1494fa6299
triumphcreates <triumphcreates@gmail.com>
2024-07-08 20:06:02 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+
+C = 0;
+while
+(c < 10) {
+putchar(c + '0');
+c++;
+}
+putchar('\n');
+return (0);
+}
